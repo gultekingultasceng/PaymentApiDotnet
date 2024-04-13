d57c3924464f9c5d6e4c7994daa26d5c5c91dbb7
Gultekin Gultas <gultekingultas123@gmail.com>
2024-04-14 01:26:25 +0300

    postgresql datacontext created

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentApiDotnet.Helpers
+{
+    public class DataContext : DbContext
+    {
+
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<CardOwner> CardOwners { get; set; }
+    }
+}
